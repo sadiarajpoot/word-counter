f7c76c44b262d00479c49be8ac89482cf02c97ae
Sadia Rajpoot <sadiakham985@gmail.com>
2024-04-20 07:15:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+console.log("=".repeat(70));
+console.log(chalk.bold.yellowBright("\n\t\t WELLCOME TO WORD COUNTER BY SADIA KHAN"));
+console.log("=".repeat(70));
+import inquirer from "inquirer";
+import chalk from "chalk";
+//from the user to  enter a sentence
+let answers = await inquirer.prompt([{
+        name: "sentence", type: "input", message: "Enter a sentence "
+    }
+]);
+//trimming the sentences and splitting it into words based on "space"
+let words = answers.sentence.trim().split(" ");
+//analysis of use input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("-sentence words:"));
+console.log(words);
+console.log(chalk.bold(`\n - word count:${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
